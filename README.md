318e8368c1880fd0619a66480eb5d14e94d6b609
Rex <rex.fenley@gmail.com>
2022-07-21 17:10:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# shaped-array
---
+++ Tests/shaped-arrayTests/shaped_arrayTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import shaped_array
+
+final class shaped_arrayTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        print(ShapedArray())
+    }
+}
